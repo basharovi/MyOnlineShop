b80f0fa47168731708e9ce50fe666db47f87b85f
basharovi <hello@basharovi.tech>
2020-06-29 14:14:11 +0600

    Added a Migration for ProductCategoryModel & Updated DB

--- MyOnlineShop/Data/ApplicationDbContext.cs
+++ MyOnlineShop/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyOnlineShop.Models;
 
 namespace MyOnlineShop.Data
 {
@@ -12,5 +13,7 @@ namespace MyOnlineShop.Data
             : base(options)
         {
         }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
     }
 }
---
+++ MyOnlineShop/Models/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyOnlineShop.Models
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+    }
+}
