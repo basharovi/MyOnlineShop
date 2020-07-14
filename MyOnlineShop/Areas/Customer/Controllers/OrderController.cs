8d6617316874827eb313c0973a731970ed8e7cbd
basharovi <hello@basharovi.tech>
2020-07-14 23:25:47 +0600

    Added Checkout View

---
+++ MyOnlineShop/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyOnlineShop.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class OrderController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
