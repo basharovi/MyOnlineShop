0cab21314c27a820b8451062f7a88f246ac072bd
basharovi <hello@basharovi.tech>
2020-07-24 02:48:50 +0600

    Added UserRegister

---
+++ MyOnlineShop/Areas/Customer/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyOnlineShop.Models;
+
+namespace MyOnlineShop.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(ApplicationUser user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var result = await _userManager.CreateAsync(user);
+
+            if (result.Succeeded)
+            {
+                return Redirect("/Identity/Account/Login");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+
+            return View(user);
+        }
+    }
+}
--- MyOnlineShop/Models/ApplicationUser.cs
+++ MyOnlineShop/Models/ApplicationUser.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.ComponentModel;
 using Microsoft.AspNetCore.Identity;
 
 namespace MyOnlineShop.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
 
         public DateTime CreatedDate { get; set; } = DateTime.Now;
