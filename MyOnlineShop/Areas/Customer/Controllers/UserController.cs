2ec55afe8f6de3156d5b16554e6f49eeb43e6eef
basharovi <hello@basharovi.tech>
2020-07-24 22:04:10 +0600

    Added User Update & Delete

--- MyOnlineShop/Areas/Customer/Controllers/UserController.cs
+++ MyOnlineShop/Areas/Customer/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using MyOnlineShop.Data;
 using MyOnlineShop.Models;
 
@@ -61,5 +62,65 @@ namespace MyOnlineShop.Areas.Customer.Controllers
             return View(_db.Users);
         }
 
+        public async Task<IActionResult> Update(string id)
+        {
+            var user = await _db.Users.FindAsync(id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(ApplicationUser user)
+        {
+            var userInfo = await _db.Users.FindAsync(user.Id);
+
+            if (userInfo is null)
+            {
+                return NotFound();
+            }
+
+            userInfo.FirstName = user.FirstName;
+            userInfo.LastName = user.LastName;
+            userInfo.Email = user.Email;
+            userInfo.PhoneNumber = user.PhoneNumber;
+
+            var result = await _userManager.UpdateAsync(userInfo);
+
+            if (result.Succeeded)
+            {
+                TempData["crudMessage"] = "User has been Updated Successfully!";
+
+                return Redirect("/User/List");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+
+            return View(user);
+        }
+
+        public IActionResult Delete(string id)
+        {
+            var userInfo = _db.Users.Find(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            _db.Users.Remove(userInfo);
+            _db.SaveChanges();
+
+            TempData["crudMessage"] = "User has been Deleted Successfully!";
+
+            return Redirect("/User/List");
+        }
     }
 }
