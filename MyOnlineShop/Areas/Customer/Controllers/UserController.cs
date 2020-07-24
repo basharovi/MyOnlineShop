398f071ce7a809dce169f8b28df69e08c29e26a6
basharovi <hello@basharovi.tech>
2020-07-24 20:17:42 +0600

    Added UserList

--- MyOnlineShop/Areas/Customer/Controllers/UserController.cs
+++ MyOnlineShop/Areas/Customer/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MyOnlineShop.Data;
 using MyOnlineShop.Models;
 
 namespace MyOnlineShop.Areas.Customer.Controllers
@@ -9,10 +10,16 @@ namespace MyOnlineShop.Areas.Customer.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _db;
 
-        public UserController(UserManager<IdentityUser> userManager)
+        public UserController
+        (
+            UserManager<IdentityUser> userManager,
+            ApplicationDbContext db
+        )
         {
             _userManager = userManager;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -26,6 +33,7 @@ namespace MyOnlineShop.Areas.Customer.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(ApplicationUser user)
         {
             if (!ModelState.IsValid)
@@ -47,5 +55,11 @@ namespace MyOnlineShop.Areas.Customer.Controllers
 
             return View(user);
         }
+
+        public IActionResult List()
+        {
+            return View(_db.Users);
+        }
+
     }
 }
